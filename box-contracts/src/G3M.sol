796b2ea7d1d874cc8e55459e699e2bac024f5a76
clemlak <clementlakhal@gmail.com>
2023-09-19 18:02:59 +0400

    feat: add IG3M interface

--- box-contracts/src/G3M.sol
+++ box-contracts/src/G3M.sol
@@ -1,26 +1,12 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+import "./IG3M.sol";
+
 interface IERC20 {
     function transfer(address to, uint256 amount) external returns (bool);
     function transferFrom(address from, address to, uint256 amount) external returns (bool);
     function balanceOf(address account) external view returns (uint256);
-}
-
-interface IG3M {
-    event AddLiquidity(address indexed sender, uint256 liquidity, uint256 amountX, uint256 amountY);
-    event RemoveLiquidity(address indexed sender, uint256 liquidity, uint256 amountX, uint256 amountY);
-    event Swap(address indexed sender, bool swapDirection, uint256 input, uint256 output);
-    event UpdateWeight(uint256 oldPrimaryWeight, uint256 newPrimaryWeight);
-
-    function addLiquidity(uint256 liquidity) external returns (uint256 amountX, uint256 amountY);
-    function removeLiquidity(uint256 liquidity) external returns (uint256 amountX, uint256 amountY);
-    function swap(bool swapDirection, uint256 input, uint256 output) external;
-    function updateWeight(uint256 newPrimaryWeight) external;
-    function getSpotPriceX() external view returns (uint256);
-    function getSpotPriceY() external view returns (uint256);
-    function getPrimaryWeight() external view returns (uint256);
-    function getSecondaryWeight() external view returns (uint256);
 }
 
 contract G3M is IG3M {
---
+++ box-contracts/src/IG3M.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+interface IG3M {
+    event AddLiquidity(address indexed sender, uint256 liquidity, uint256 amountX, uint256 amountY);
+    event RemoveLiquidity(address indexed sender, uint256 liquidity, uint256 amountX, uint256 amountY);
+    event Swap(address indexed sender, bool swapDirection, uint256 input, uint256 output);
+    event UpdateWeight(uint256 oldPrimaryWeight, uint256 newPrimaryWeight);
+
+    function addLiquidity(uint256 liquidity) external returns (uint256 amountX, uint256 amountY);
+    function removeLiquidity(uint256 liquidity) external returns (uint256 amountX, uint256 amountY);
+    function swap(bool swapDirection, uint256 input, uint256 output) external;
+    function updateWeight(uint256 newPrimaryWeight) external;
+    function getSpotPriceX() external view returns (uint256);
+    function getSpotPriceY() external view returns (uint256);
+    function getPrimaryWeight() external view returns (uint256);
+    function getSecondaryWeight() external view returns (uint256);
+}
