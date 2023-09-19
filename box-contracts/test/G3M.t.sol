33587af3fb01c1d5641f01daf85fcd5732846577
clemlak <clementlakhal@gmail.com>
2023-09-19 18:21:08 +0400

    test: add test_updatePrimaryWeight

--- box-contracts/test/G3M.t.sol
+++ box-contracts/test/G3M.t.sol
@@ -16,6 +16,13 @@ contract G3MTest is Test {
         g3m = new G3M(address(tokenX), address(tokenY), 0.5 ether);
     }
 
+    function test_updatePrimaryWeight(uint256 newPrimaryWeight) public {
+        vm.assume(newPrimaryWeight <= g3m.WAD());
+        g3m.updatePrimaryWeight(newPrimaryWeight);
+        assertEq(g3m.getPrimaryWeight(), newPrimaryWeight);
+        assertEq(g3m.getSecondaryWeight(), g3m.WAD() - newPrimaryWeight);
+    }
+
     function test_addLiquidity() public {
         tokenX.mint(address(this), 100 ether);
         tokenY.mint(address(this), 100 ether);
@@ -29,5 +36,17 @@ contract G3MTest is Test {
         assertEq(tokenX.balanceOf(address(this)), 95 ether);
         assertEq(tokenY.balanceOf(address(this)), 95 ether);
         assertEq(g3m.balanceOf(address(this)), 10 ether);
+    }
+
+    function test_removeLiquidity() public {
+        tokenX.mint(address(this), 100 ether);
+        tokenY.mint(address(this), 100 ether);
+        tokenX.approve(address(g3m), 100 ether);
+        tokenY.approve(address(g3m), 100 ether);
+
+        g3m.addLiquidity(10 ether);
+        g3m.removeLiquidity(5 ether);
+
+        assertEq(g3m.balanceOf(address(this)), 5 ether);
     }
 }
