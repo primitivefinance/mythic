ebd54b146e26607e221b4c471c03389ad3bbf7de
clemlak <clementlakhal@gmail.com>
2023-09-19 18:00:32 +0400

    feat: complete wip implementation

--- box-contracts/src/G3M.sol
+++ box-contracts/src/G3M.sol
@@ -8,25 +8,30 @@ interface IERC20 {
 }
 
 interface IG3M {
-    event AddLiquidity(address indexed sender, uint256 liquidity, uint256 amount1, uint256 amount2);
-    event RemoveLiquidity(address indexed sender, uint256 liquidity, uint256 amount1, uint256 amount2);
-    event Swap(address indexed sender, bool swapDirection, uint256 amount);
+    event AddLiquidity(address indexed sender, uint256 liquidity, uint256 amountX, uint256 amountY);
+    event RemoveLiquidity(address indexed sender, uint256 liquidity, uint256 amountX, uint256 amountY);
+    event Swap(address indexed sender, bool swapDirection, uint256 input, uint256 output);
     event UpdateWeight(uint256 oldPrimaryWeight, uint256 newPrimaryWeight);
 
-    function addLiquidity(uint256 liquidity) external returns (uint256 amount1, uint256 amount2);
-    function removeLiquidity(uint256 liquidity) external returns (uint256 amount1, uint256 amount2);
-    function swap(bool swapDirection, uint256 amount) external;
+    function addLiquidity(uint256 liquidity) external returns (uint256 amountX, uint256 amountY);
+    function removeLiquidity(uint256 liquidity) external returns (uint256 amountX, uint256 amountY);
+    function swap(bool swapDirection, uint256 input, uint256 output) external;
     function updateWeight(uint256 newPrimaryWeight) external;
-    function getSpotPrice() external view returns (uint256);
+    function getSpotPriceX() external view returns (uint256);
+    function getSpotPriceY() external view returns (uint256);
     function getPrimaryWeight() external view returns (uint256);
     function getSecondaryWeight() external view returns (uint256);
 }
 
 contract G3M is IG3M {
     address public admin;
-    address public token1;
-    address public token2;
+    address public tokenX;
+    address public tokenY;
     uint256 public primaryWeight;
+    uint256 public reserveX;
+    uint256 public reserveY;
+
+    uint256 public constant WAD = 1 ether;
 
     mapping(address => uint256) public balanceOf;
 
@@ -35,32 +40,57 @@ contract G3M is IG3M {
         _;
     }
 
-    constructor(address token1_, address token2_, uint256 primaryWeight_) {
-        token1 = token1_;
-        token2 = token2_;
+    constructor(address tokenX_, address tokenY_, uint256 primaryWeight_) {
+        require(tokenX != tokenY, "Invalid tokens");
+        require(primaryWeight_ <= WAD, "Invalid weight");
+        tokenX = tokenX_;
+        tokenY = tokenY_;
         primaryWeight = primaryWeight_;
     }
 
-    function addLiquidity(uint256 liquidity) external returns (uint256 amount1, uint256 amount2) {
-        (amount1, amount2) = calculateAmounts(liquidity);
-
-        IERC20(token1).transferFrom(msg.sender, address(this), amount1);
-        IERC20(token2).transferFrom(msg.sender, address(this), amount2);
-
-        balanceOf[msg.sender] += liquidity;
-        emit AddLiquidity(msg.sender, liquidity, amount1, amount2);
+    function updateWeight(uint256 newPrimaryWeight) external onlyAdmin {
+        emit UpdateWeight(primaryWeight, newPrimaryWeight);
+        primaryWeight = newPrimaryWeight;
     }
 
-    function removeLiquidity(uint256 liquidity) external returns (uint256 amount1, uint256 amount2) {
+    function addLiquidity(uint256 liquidity) external returns (uint256 amountX, uint256 amountY) {
+        (amountX, amountY) = calculateAmounts(liquidity);
+
+        IERC20(tokenX).transferFrom(msg.sender, address(this), amountX);
+        IERC20(tokenY).transferFrom(msg.sender, address(this), amountY);
+
+        reserveX += amountX * WAD;
+        reserveY += amountY * WAD;
+
+        balanceOf[msg.sender] += liquidity;
+        emit AddLiquidity(msg.sender, liquidity, amountX, amountY);
+    }
+
+    function removeLiquidity(uint256 liquidity) external returns (uint256 amountX, uint256 amountY) {
         require(balanceOf[msg.sender] >= liquidity, "Insufficient liquidity");
         balanceOf[msg.sender] -= liquidity;
 
-        (amount1, amount2) = calculateAmounts(liquidity);
+        (amountX, amountY) = calculateAmounts(liquidity);
 
-        IERC20(token1).transfer(msg.sender, amount1);
-        IERC20(token2).transfer(msg.sender, amount2);
+        reserveX -= amountX * WAD;
+        reserveY -= amountY * WAD;
 
-        emit RemoveLiquidity(msg.sender, liquidity, amount1, amount2);
+        IERC20(tokenX).transfer(msg.sender, amountX);
+        IERC20(tokenY).transfer(msg.sender, amountY);
+
+        emit RemoveLiquidity(msg.sender, liquidity, amountX, amountY);
+    }
+
+    function swap(bool swapDirection, uint256 input, uint256 output) external {
+        IERC20(swapDirection ? tokenX : tokenY).transferFrom(msg.sender, address(this), input);
+        IERC20(swapDirection ? tokenX : tokenY).transfer(msg.sender, output);
+
+        reserveX += (swapDirection ? input : output) * WAD;
+        reserveY += (swapDirection ? output : input) * WAD;
+
+        require((reserveX * WAD) / reserveY == primaryWeight, "Invalid swap");
+
+        emit Swap(msg.sender, swapDirection, input, output);
     }
 
     function updateWeights(uint256 newPrimaryWeight) external onlyAdmin {
@@ -73,11 +103,19 @@ contract G3M is IG3M {
     }
 
     function getSecondaryWeight() external view returns (uint256) {
-        return 10_000 - primaryWeight;
+        return WAD - primaryWeight;
     }
 
-    function calculateAmounts(uint256 liquidity) public returns (uint256 amount1, uint256 amount2) {
-        amount1 = liquidity * primaryWeight / 10_000;
-        amount2 = liquidity - amount1;
+    function getSpotPriceX() external view returns (uint256) {
+        return reserveY / reserveX;
+    }
+
+    function getSpotPriceY() external view returns (uint256) {
+        return reserveX / reserveY;
+    }
+
+    function calculateAmounts(uint256 liquidity) public view returns (uint256 amountX, uint256 amountY) {
+        amountX = (liquidity * primaryWeight) / WAD;
+        amountY = liquidity - amountX;
     }
 }
