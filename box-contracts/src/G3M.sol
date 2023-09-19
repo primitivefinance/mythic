2a70e0da749bbc98cd9f6acdd8ee787b4ff9fa26
clemlak <clementlakhal@gmail.com>
2023-09-19 12:00:49 +0400

    feat: add interface, wip implementation

--- box-contracts/src/G3M.sol
+++ box-contracts/src/G3M.sol
@@ -5,15 +5,35 @@ interface IG3M {
     event AddLiquidity(address indexed sender, uint256 liquidity);
     event RemoveLiquidity(address indexed sender, uint256 liquidity);
     event Swap(address indexed sender, bool swapDirection, uint256 amount);
-    event UpdateWeight(address indexed sender, uint256 oldWeight, uint256 newWeight);
+    event UpdateWeight(uint256 oldPrimaryWeight, uint256 newPrimaryWeight);
 
     function addLiquidity(uint256 liquidity) external;
     function removeLiquidity(uint256 liquidity) external;
     function swap(bool swapDirection, uint256 amount) external;
-    function updateWeight(uint256 newWeight) external;
+    function updateWeight(uint256 newPrimaryWeight) external;
     function getSpotPrice() external view returns (uint256);
     function getPrimaryWeight() external view returns (uint256);
     function getSecondaryWeight() external view returns (uint256);
 }
 
-contract G3M {}
+contract G3M is IG3M {
+    address public admin;
+    address public token1;
+    address public token2;
+    uint256 public primaryWeight;
+    uint256 public secondaryWeight;
+
+    mapping(address => uint256) public balanceOf;
+
+    modifier onlyAdmin() {
+        require(msg.sender == admin, "Not admin");
+        _;
+    }
+
+    constructor(address token1_, address token2_, uint256 primaryWeight_) {
+        token1 = token1_;
+        token2 = token2_;
+        primaryWeight = primaryWeight_;
+        secondaryWeight = 10_000 - primaryWeight;
+    }
+}
