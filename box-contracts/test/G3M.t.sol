5348a3f538d3fd4cc31fb77b170168330eb91028
clemlak <clementlakhal@gmail.com>
2023-09-19 18:13:21 +0400

    test: add test_addLiquidity

--- box-contracts/test/G3M.t.sol
+++ box-contracts/test/G3M.t.sol
@@ -2,10 +2,32 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
+import "solmate/test/utils/mocks/MockERC20.sol";
 import "../src/G3M.sol";
 
 contract G3MTest is Test {
     G3M public g3m;
+    MockERC20 public tokenX;
+    MockERC20 public tokenY;
 
-    function setUp() public {}
+    function setUp() public {
+        tokenX = new MockERC20("TokenX", "X", 18);
+        tokenY = new MockERC20("TokenY", "Y", 18);
+        g3m = new G3M(address(tokenX), address(tokenY), 0.5 ether);
+    }
+
+    function test_addLiquidity() public {
+        tokenX.mint(address(this), 100 ether);
+        tokenY.mint(address(this), 100 ether);
+        tokenX.approve(address(g3m), 100 ether);
+        tokenY.approve(address(g3m), 100 ether);
+
+        g3m.addLiquidity(10 ether);
+
+        assertEq(tokenX.balanceOf(address(g3m)), 5 ether);
+        assertEq(tokenY.balanceOf(address(g3m)), 5 ether);
+        assertEq(tokenX.balanceOf(address(this)), 95 ether);
+        assertEq(tokenY.balanceOf(address(this)), 95 ether);
+        assertEq(g3m.balanceOf(address(this)), 10 ether);
+    }
 }
