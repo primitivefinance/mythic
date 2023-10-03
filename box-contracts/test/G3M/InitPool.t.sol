8e467f0bf41b1504cd19986039f29cb676ef5f9a
clemlak <clementlakhal@gmail.com>
2023-10-03 16:21:35 +0400

    test: update InitPool

--- box-contracts/test/G3M/InitPool.t.sol
+++ box-contracts/test/G3M/InitPool.t.sol
@@ -8,24 +8,22 @@ contract InitPool is SetUp {
         uint256 amountX = 750 ether;
         uint256 amountY = 250 ether;
 
-        uint256 invariant =
-            computeInvariantDown(amountX, 0.5 ether, amountY, 0.5 ether);
-        uint256 liquidity = g3m.initPool(amountX, amountY);
+        UD60x18 invariant = computeInvariant(
+            convert(amountX), ud(0.5 ether), convert(amountY), ud(0.5 ether)
+        );
+        UD60x18 liquidity = g3m.initPool(amountX, amountY);
 
         assertEq(tokenX.balanceOf(address(g3m)), amountX);
         assertEq(tokenY.balanceOf(address(g3m)), amountY);
-        assertEq(g3m.reserveX(), amountX * 10 ** 18);
-        assertEq(g3m.reserveY(), amountY * 10 ** 18);
+        assertEq(g3m.reserveX(), convert(amountX));
+        assertEq(g3m.reserveY(), convert(amountY));
         assertEq(g3m.totalLiquidity(), liquidity + BURNT_LIQUIDITY);
-        assertEq(g3m.getSpotPrice(), toWad(3));
+        assertEq(g3m.getSpotPrice(), 3 ether);
         assertEq(g3m.balanceOf(address(this)), liquidity);
-        assertEq(g3m.balanceOf(address(0)), 1_000);
-
-        // TODO: Not a huge fan of using approx, let's see if we can use
-        // something better here.
-        assertApproxEqRel(g3m.totalLiquidity(), invariant * 2, 1_000);
-        assertApproxEqRel(
-            g3m.balanceOf(address(this)), invariant * 2 - BURNT_LIQUIDITY, 1_000
+        assertEq(g3m.balanceOf(address(0)), BURNT_LIQUIDITY);
+        assertEq(g3m.totalLiquidity(), invariant * ud(2));
+        assertEq(
+            g3m.balanceOf(address(this)), invariant * ud(2) - BURNT_LIQUIDITY
         );
     }
 
