b260fefe17aae09dac2e566c371a0ebea106086e
clemlak <clementlakhal@gmail.com>
2023-10-03 15:26:34 +0400

    test: add ComputesTokenYAmountIn

--- box-contracts/test/G3MMath/ComputeInvariant.t.sol
+++ box-contracts/test/G3MMath/ComputeInvariant.t.sol
@@ -2,12 +2,17 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
-import "../../src/lib/G3MMath.sol";
+import "../../src/lib/G3MPRBMath.sol";
 
 contract ComputeInvariant is Test {
     function test_computeInvariant_ComputesInvariant() public view {
-        uint256 invariant =
-            computeInvariantUp(750 ether, 0.5 ether, 250 ether, 0.5 ether);
-        console.log(invariant);
+        UD60x18 reserveX = convert(750);
+        UD60x18 reserveY = convert(250);
+        UD60x18 weightX = ud(0.5 ether);
+        UD60x18 weightY = ud(0.5 ether);
+
+        UD60x18 invariant =
+            computeInvariant(reserveX, weightX, reserveY, weightY);
+        console.log(UD60x18.unwrap(invariant));
     }
 }
