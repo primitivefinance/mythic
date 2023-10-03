4377eb40bfa17a26c86c41f2c7c512dde08fd953
clemlak <clementlakhal@gmail.com>
2023-10-03 15:54:57 +0400

    test: update ComputeOutGivenIn tests

--- box-contracts/test/G3MMath/ComputeOutGivenIn.t.sol
+++ box-contracts/test/G3MMath/ComputeOutGivenIn.t.sol
@@ -2,18 +2,18 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
-import "../../src/lib/G3MMath.sol";
+import "../../src/lib/G3MPRBMath.sol";
 
 contract ComputeOutGivenIn is Test {
     function test_computeOutGivenIn_ComputesAmountOut() public view {
-        uint256 amountOut = computeOutGivenIn(
-            toWad(50 ether),
-            toWad(750 ether),
-            toWad(250 ether),
-            0.5 ether,
-            0.5 ether
-        );
+        uint256 amountIn = 50 ether;
+        UD60x18 reserveX = convert(750 ether);
+        UD60x18 reserveY = convert(250 ether);
+        UD60x18 weightX = ud(0.5 ether);
+        UD60x18 weightY = ud(0.5 ether);
 
+        uint256 amountOut =
+            computeOutGivenIn(amountIn, reserveX, weightX, reserveY, weightY);
         console.log(amountOut);
     }
 }
