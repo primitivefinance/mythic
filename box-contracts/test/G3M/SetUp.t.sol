0ed4089704beb2202c7dd72ab4a29db20f9368d7
clemlak <clementlakhal@gmail.com>
2023-10-03 16:06:58 +0400

    test: use G3MTest helper contract

--- box-contracts/test/G3M/SetUp.t.sol
+++ box-contracts/test/G3M/SetUp.t.sol
@@ -1,11 +1,12 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import "forge-std/Test.sol";
 import "solmate/test/utils/mocks/MockERC20.sol";
+
+import "../G3MTest.t.sol";
 import "../../src/G3M.sol";
 
-contract SetUp is Test {
+contract SetUp is G3MTest {
     G3M public g3m;
     MockERC20 public tokenX;
     MockERC20 public tokenY;
@@ -17,7 +18,7 @@ contract SetUp is Test {
         tokenX.mint(address(this), 20000 ether);
         tokenY.mint(address(this), 20000 ether);
 
-        g3m = new G3M(address(tokenX), address(tokenY), 0.5 ether);
+        g3m = new G3M(address(tokenX), address(tokenY), ud(0.5 ether));
 
         tokenX.approve(address(g3m), 20000 ether);
         tokenY.approve(address(g3m), 20000 ether);
