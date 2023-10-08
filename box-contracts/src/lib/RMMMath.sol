a058f4d8981205e39f70a92b63101ab7b9461312
clemlak <clementlakhal@gmail.com>
2023-10-08 13:55:34 +0400

    feat: update computeLGivenX

--- box-contracts/src/lib/RMMMath.sol
+++ box-contracts/src/lib/RMMMath.sol
@@ -7,6 +7,8 @@ uint256 constant ONE = 1e18;
 
 uint256 constant HALF = 0.5e18;
 
+uint256 constant TWO = 2e18;
+
 function toWad(uint256 a) pure returns (uint256) {
     return a * ONE;
 }
@@ -21,20 +23,13 @@ function computeLGivenX(
     uint256 K,
     uint256 sigma
 ) pure returns (uint256 L) {
-    uint256 lnSK = uint256(
-        FixedPointMathLib.lnWad(int256(FixedPointMathLib.divWadUp(S, K)))
+    int256 lnSDivK =
+        FixedPointMathLib.lnWad(int256(FixedPointMathLib.divWadUp(S, K)));
+    uint256 halfSigmaPowTwo = FixedPointMathLib.mulWadUp(
+        HALF, uint256(FixedPointMathLib.powWad(int256(sigma), int256(TWO)))
     );
-    uint256 halfSigmaPow = FixedPointMathLib.mulWadUp(
-        HALF, uint256(FixedPointMathLib.powWad(int256(sigma), int256(2)))
-    );
-
-    L = FixedPointMathLib.divWadUp(
-        toWad(x),
-        ONE
-            - uint256(
-                Gaussian.cdf(
-                    int256(FixedPointMathLib.divWadUp(lnSK + halfSigmaPow, sigma))
-                )
-            )
-    );
+    int256 cdf =
+        Gaussian.cdf((lnSDivK + int256(halfSigmaPowTwo)) * 1e18 / int256(sigma));
+    int256 denominator = int256(1e18) - cdf;
+    L = FixedPointMathLib.divWadUp(x, uint256(denominator));
 }
