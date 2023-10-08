9ef50a179b6693706535e0632ec67249fa4a0aff
clemlak <clementlakhal@gmail.com>
2023-10-08 19:00:23 +0400

    test: computeLGivenY and computeXGivenL

--- box-contracts/test/RMMMath.t.sol
+++ box-contracts/test/RMMMath.t.sol
@@ -6,12 +6,13 @@ import "forge-std/Test.sol";
 import "../src/lib/RMMMath.sol";
 
 contract RMMMathTest is Test {
-    uint256 x = 5000 ether;
     uint256 K = 2000 ether;
     uint256 S = 1800 ether;
     int256 sigma = 0.05 ether;
 
     function test_computeLGivenX() public view {
+        uint256 x = 5000 ether;
+
         // console.log(computeLGivenX(x, S, K, uint256(sigma)));
 
         int256 lnSDivK =
@@ -35,6 +36,7 @@ contract RMMMathTest is Test {
     }
 
     function test_computeYGivenL() public view {
+        uint256 x = 5000 ether;
         uint256 L = computeLGivenX(x, S, K, uint256(sigma));
         console.log(L);
 
@@ -55,5 +57,47 @@ contract RMMMathTest is Test {
             K, FixedPointMathLib.mulWadUp(L, uint256(cdf))
         );
         console.log(y);
+    }
+
+    function test_computeLGivenY() public view {
+        uint256 y = 5000 ether;
+
+        int256 lnSDivK =
+            FixedPointMathLib.lnWad(int256(FixedPointMathLib.divWadUp(S, K)));
+        console.logInt(lnSDivK);
+        uint256 halfSigmaPowTwo = FixedPointMathLib.mulWadUp(
+            HALF, uint256(FixedPointMathLib.powWad(int256(sigma), int256(TWO)))
+        );
+        console.log(halfSigmaPowTwo);
+        int256 minus = lnSDivK - int256(halfSigmaPowTwo);
+        console.logInt(minus);
+        int256 divSigma = minus * 1e18 / int256(sigma);
+        console.logInt(divSigma);
+        int256 cdf = Gaussian.cdf(divSigma);
+        console.logInt(cdf);
+        uint256 L = FixedPointMathLib.divWadUp(
+            y, FixedPointMathLib.mulWadUp(K, uint256(cdf))
+        );
+        console.log(L);
+    }
+
+    function test_computeXGivenL() public view {
+        uint256 L = 151 ether;
+
+        int256 lnSDivK =
+            FixedPointMathLib.lnWad(int256(FixedPointMathLib.divWadUp(S, K)));
+        console.logInt(lnSDivK);
+        uint256 halfSigmaPowTwo = FixedPointMathLib.mulWadUp(
+            HALF, uint256(FixedPointMathLib.powWad(int256(sigma), int256(TWO)))
+        );
+        console.log(halfSigmaPowTwo);
+        int256 plus = lnSDivK + int256(halfSigmaPowTwo);
+        console.logInt(plus);
+        int256 divSigma = plus * 1e18 / int256(sigma);
+        console.logInt(divSigma);
+        int256 cdf = Gaussian.cdf(divSigma);
+        console.logInt(cdf);
+        uint256 x = FixedPointMathLib.mulWadUp(L, uint256(int256(ONE) - cdf));
+        console.log(x);
     }
 }
