e99ee268a59fe9f75d6f71b3d557a00e5929387b
clemlak <clementlakhal@gmail.com>
2023-10-09 16:07:10 +0400

    feat: add sigma variable

--- box-contracts/src/RMM.sol
+++ box-contracts/src/RMM.sol
@@ -4,13 +4,16 @@ pragma solidity ^0.8.13;
 import "./lib/RMMMath.sol";
 
 contract RMM {
+    uint256 public sigma;
     address public tokenX;
     address public tokenY;
     uint256 public reserveX;
     uint256 public reserveY;
+    uint256 public liquidity;
 
-    constructor(address tokenX_, address tokenY_) {
+    constructor(address tokenX_, address tokenY_, uint256 sigma_) {
         tokenX = tokenX_;
         tokenY = tokenY_;
+        sigma = sigma_;
     }
 }
