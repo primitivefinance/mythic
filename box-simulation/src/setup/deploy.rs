dea68ea2d968f6abf7d5e50970e41ffcc31a1485
Kinrezc <matt.czernik@gmail.com>
2023-10-10 11:16:41 -0400

    use derive deploy

--- box-simulation/src/setup/deploy.rs
+++ box-simulation/src/setup/deploy.rs
@@ -4,61 +4,73 @@ use arbiter_core::bindings::liquid_exchange::LiquidExchange;
 use arbiter_core::data_collection::EventLogger;
 use arbiter_core::environment::Environment;
 use arbiter_core::middleware::RevmMiddleware;
+use arbiter_derive::Deploy;
 use bindings::g3m::G3M;
 use ethers::types::U256;
+use ethers::prelude::*;
 use ethers::utils::parse_ether;
+use std::sync::Arc;
 
-pub struct ContractInstances {
-    pub arbx: ArbiterToken<RevmMiddleware>,
-    pub arby: ArbiterToken<RevmMiddleware>,
-    pub g3m: G3M<RevmMiddleware>,
-    pub lex: LiquidExchange<RevmMiddleware>,
+#[derive(Deploy)]
+pub struct Tokens {
+    pub arbx: ContractDeploymentTx<Arc<RevmMiddleware>, RevmMiddleware, ArbiterToken<RevmMiddleware>>,
+    pub arby: ContractDeploymentTx<Arc<RevmMiddleware>, RevmMiddleware, ArbiterToken<RevmMiddleware>>,
 }
 
-pub async fn deploy_contracts(env: &Environment) -> Result<ContractInstances, anyhow::Error> {
+#[derive(Deploy)]
+pub struct Exchanges {
+    pub g3m: ContractDeploymentTx<Arc<RevmMiddleware>, RevmMiddleware, G3M<RevmMiddleware>>,
+    pub lex: ContractDeploymentTx<Arc<RevmMiddleware>, RevmMiddleware, LiquidExchange<RevmMiddleware>>,
+}
+
+pub struct Contracts {
+    pub tokens: TokensDeployed,
+    pub exchanges: ExchangesDeployed,
+}
+
+pub async fn deploy_contracts(env: &Environment) -> Result<Contracts, anyhow::Error> {
     let deployer = RevmMiddleware::new(&env, "deployer".into())?;
+    let decimals = u8::from(18);
+    let arbx_args = (
+        "Arbiter Token X".to_string(),
+        "arbx".to_string(),
+        decimals,
+    );
+    let arby_args = (
+        "Arbiter Token Y".to_string(),
+        "arby".to_string(),
+        decimals,
+    );
 
-    let arbx = ArbiterToken::deploy(
-        deployer.clone(),
-        ("Arbiter Token X".to_string(), "arbx".to_string(), u8::from(18)),
-    )?
-    .send()
-    .await?;
-    let arby = ArbiterToken::deploy(
-        deployer.clone(),
-        ("Arbiter Token Y".to_string(), "arby".to_string(), u8::from(18)),
-    )?
-    .send()
-    .await?;
+    let tokens = Tokens {
+        arbx: ArbiterToken::deploy(deployer.clone(), arbx_args)?,
+        arby: ArbiterToken::deploy(deployer.clone(), arby_args)?,
+    }.deploy().await?;
 
-    let g3m = G3M::deploy(
-        deployer.clone(),
-        (
-            arbx.address(),
-            arby.address(),
-            U256::from(10u64.pow(18) / 2),
-        ),
-    )?
-    .send()
-    .await?;
-    let lex = LiquidExchange::deploy(
-        deployer.clone(),
-        (arbx.address(), arby.address(), parse_ether(1).unwrap()),
-    )?
-    .send()
-    .await?;
 
-    EventLogger::builder()
-        .add(arbx.events(), "arbx")
-        .add(arby.events(), "arby")
-        .run()
-        .await?;
+    let g3m_args = (
+        tokens.arbx.address(),
+        tokens.arby.address(),
+        U256::from(10u64.pow(18) / 2),
+    );
+
+    let lex_args = (
+        tokens.arbx.address(),
+        tokens.arby.address(),
+        parse_ether(1).unwrap(),
+    );
+
+    let exchanges = Exchanges {
+        g3m: G3M::deploy(deployer.clone(), g3m_args)?,
+        lex: LiquidExchange::deploy(deployer.clone(),lex_args)?,
+    }.deploy().await?;
+
+    let contracts = Contracts {
+        tokens,
+        exchanges,
+    };
+
     // let g3m_listener = EventLogger::builder().add(g3m.events(), "g3m").run().await?;
 
-    Ok(ContractInstances {
-        arbx,
-        arby,
-        g3m,
-        lex,
-    })
+    Ok(contracts)
 }
