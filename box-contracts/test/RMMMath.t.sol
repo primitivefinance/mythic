f160e18be35a4b574ef3fc285c1944b11c580379
clemlak <clementlakhal@gmail.com>
2023-10-12 14:51:26 +0400

    test: update RMMMathTest

--- box-contracts/test/RMMMath.t.sol
+++ box-contracts/test/RMMMath.t.sol
@@ -8,136 +8,48 @@ import "../src/lib/RMMMath.sol";
 contract RMMMathTest is Test {
     uint256 S = 2000 ether;
     uint256 K = 1800 ether;
-    int256 sigma = 0.05 ether;
+    uint256 sigma = 0.05 ether;
+
+    function test_compute_backAndForth() public view {
+        uint256 x = 5_000 ether;
+        uint256 L = computeLGivenX(x, S, K, sigma);
+        uint256 y = computeYGivenL(L, S, K, sigma);
+
+        console.log("y", y);
+        console.log("L1", L);
+        console.log("L2", computeLGivenY(y, S, K, sigma));
+        console.log("x2", computeXGivenL(L, S, K, sigma));
+    }
 
     function test_computeLGivenX() public view {
         uint256 x = 5000 ether;
-
-        int256 lnSDivK =
-            FixedPointMathLib.lnWad(int256(FixedPointMathLib.divWadUp(S, K)));
-        console.logInt(lnSDivK);
-        uint256 halfSigmaPowTwo = FixedPointMathLib.mulWadUp(
-            HALF, uint256(FixedPointMathLib.powWad(int256(sigma), int256(TWO)))
-        );
-        console.log(halfSigmaPowTwo);
-
-        int256 plus = lnSDivK + int256(halfSigmaPowTwo);
-        console.logInt(plus);
-        int256 div = plus * 1e18 / int256(sigma);
-        console.logInt(div);
-        int256 cdf = Gaussian.cdf(div);
-        console.logInt(cdf);
-        int256 denominator = int256(1e18) - cdf;
-        console.logInt(denominator);
-        uint256 L = FixedPointMathLib.divWadUp(x, uint256(denominator));
-        console.log(L);
-
-        console.log(computeLGivenX(x, S, K, uint256(sigma)));
+        console.log(computeLGivenX(x, S, K, sigma));
     }
 
     function test_computeYGivenL() public view {
         uint256 x = 5000 ether;
-        uint256 L = computeLGivenX(x, S, K, uint256(sigma));
-        console.log(L);
-
-        int256 lnSDivK =
-            FixedPointMathLib.lnWad(int256(FixedPointMathLib.divWadUp(S, K)));
-        console.logInt(lnSDivK);
-        uint256 halfSigmaPowTwo = FixedPointMathLib.mulWadUp(
-            HALF, uint256(FixedPointMathLib.powWad(int256(sigma), int256(TWO)))
-        );
-        console.log(halfSigmaPowTwo);
-        int256 minus = lnSDivK - int256(halfSigmaPowTwo);
-        console.logInt(minus);
-        int256 div = minus * 1e18 / int256(sigma);
-        console.logInt(div);
-        int256 cdf = Gaussian.cdf(div);
-        console.logInt(cdf);
-        uint256 y = FixedPointMathLib.mulWadUp(
-            K, FixedPointMathLib.mulWadUp(L, uint256(cdf))
-        );
+        uint256 L = computeLGivenX(x, S, K, sigma);
+        uint256 y = computeYGivenL(L, S, K, sigma);
         console.log(y);
     }
 
     function test_computeLGivenY() public view {
         uint256 y = 5000 ether;
-
-        int256 lnSDivK =
-            FixedPointMathLib.lnWad(int256(FixedPointMathLib.divWadUp(S, K)));
-        console.logInt(lnSDivK);
-        uint256 halfSigmaPowTwo = FixedPointMathLib.mulWadUp(
-            HALF, uint256(FixedPointMathLib.powWad(int256(sigma), int256(TWO)))
-        );
-        console.log(halfSigmaPowTwo);
-        int256 minus = lnSDivK - int256(halfSigmaPowTwo);
-        console.logInt(minus);
-        int256 divSigma = minus * 1e18 / int256(sigma);
-        console.logInt(divSigma);
-        int256 cdf = Gaussian.cdf(divSigma);
-        console.logInt(cdf);
-        uint256 L = FixedPointMathLib.divWadUp(
-            y, FixedPointMathLib.mulWadUp(K, uint256(cdf))
-        );
-        console.log(L);
+        console.log(computeLGivenY(y, S, K, sigma));
     }
 
     function test_computeXGivenL() public view {
-        uint256 L = 151 ether;
-
-        int256 lnSDivK =
-            FixedPointMathLib.lnWad(int256(FixedPointMathLib.divWadUp(S, K)));
-        console.logInt(lnSDivK);
-        uint256 halfSigmaPowTwo = FixedPointMathLib.mulWadUp(
-            HALF, uint256(FixedPointMathLib.powWad(int256(sigma), int256(TWO)))
-        );
-        console.log(halfSigmaPowTwo);
-        int256 plus = lnSDivK + int256(halfSigmaPowTwo);
-        console.logInt(plus);
-        int256 divSigma = plus * 1e18 / int256(sigma);
-        console.logInt(divSigma);
-        int256 cdf = Gaussian.cdf(divSigma);
-        console.logInt(cdf);
-        uint256 x = FixedPointMathLib.mulWadUp(L, uint256(int256(ONE) - cdf));
-        console.log(x);
+        uint256 x = 5000 ether;
+        uint256 L = computeLGivenX(x, S, K, sigma);
+        console.log(computeXGivenL(L, S, K, sigma));
     }
 
-    function test_computeSpotPrice() public {
+    function test_computeSpotPrice() public view {
         uint256 reserveX = 5_000 ether;
         uint256 tau = 1 ether;
 
-        uint256 liquidity = computeLGivenX(reserveX, S, K, uint256(sigma));
+        uint256 liquidity = computeLGivenX(reserveX, S, K, sigma);
         console.log("liquidity:", liquidity);
-
-        uint256 halfSigmaPower2Tau = FixedPointMathLib.mulWadDown(
-            HALF,
-            FixedPointMathLib.mulWadDown(
-                uint256(FixedPointMathLib.powWad(int256(sigma), int256(TWO))),
-                tau
-            )
-        );
-        console.log("halfSigmaPower2Tau:", halfSigmaPower2Tau);
-
-        uint256 sigmaSqrtTau = FixedPointMathLib.mulWadDown(
-            uint256(sigma), FixedPointMathLib.sqrt(tau)
-        ) * 10 ** 9;
-        console.log("sigmaSqrtTau:", sigmaSqrtTau);
-
-        uint256 R1 = FixedPointMathLib.divWadDown(reserveX, liquidity);
-        console.log("R1", R1);
-
-        uint256 price = FixedPointMathLib.mulWadUp(
-            K,
-            uint256(
-                FixedPointMathLib.expWad(
-                    int256(
-                        FixedPointMathLib.mulWadDown(
-                            uint256(Gaussian.ppf(int256(ONE - R1))),
-                            sigmaSqrtTau
-                        ) - halfSigmaPower2Tau
-                    )
-                )
-            )
-        );
-        console.log("price:", price);
+        console.log(computeSpotPrice(K, sigma, reserveX, liquidity, tau));
     }
 }
