6b53c02aab5557f582007112190e44b98a71c2e0
clemlak <clementlakhal@gmail.com>
2023-10-18 16:09:32 +0400

    test: add ComputeDeltaYGivenDeltaX

---
+++ box-contracts/test/G3MMath/ComputeDeltaYGivenDeltaX.t.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "../G3MTest.t.sol";
+
+contract ComputeDeltaYGivenDeltaX is G3MTest {
+    function test_ComputeDeltaYGivenDeltaX() public {
+        UD60x18 reserveX = convert(750 ether);
+        UD60x18 reserveY = convert(250 ether);
+        uint256 deltaX = 750 ether;
+
+        uint256 deltaY = computeDeltaYGivenDeltaX(reserveX, reserveY, deltaX);
+        console.log(deltaY);
+    }
+}
