10f79d5d945f0cae1ef4ca82dd27dec4df6780ee
Colin Roberts <colin@primitive.xyz>
2023-10-27 14:08:10 -0600

    reorganizing

--- analysis/src/lib.rs
+++ analysis/src/lib.rs
@@ -1,2 +1,9 @@
+use std::{collections::BTreeMap, fs::File, io::BufReader};
+
+use anyhow::Result;
+use serde_json::{from_reader, Value};
+
+use simulation::bindings::*;
+
 #[allow(unused)]
 pub mod reader;
