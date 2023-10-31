11b1391344dad1ea93d192541900fbb42655fe6e
Colin Roberts <colin@primitive.xyz>
2023-10-31 14:27:25 -0600

    Update mod.rs

--- ui/src/analyzer/mod.rs
+++ ui/src/analyzer/mod.rs
@@ -1,18 +1,26 @@
 use arbiter_core::bindings::liquid_exchange;
 use ethers::{abi::AbiType, contract::Event};
+use iced::widget::{checkbox, Radio};
 use std::path::PathBuf;
 
 use super::*;
 
 pub struct AnalyzerApp {
-    selected_file: Option<PathBuf>,
+    selected_file: Option<usize>,
     available_files: Vec<PathBuf>,
     selected_events: Vec<liquid_exchange::LiquidExchangeEvents>,
     available_events: Vec<liquid_exchange::LiquidExchangeEvents>,
 }
 
 #[derive(Debug, Clone)]
-pub struct AnalyzerMessage {}
+pub enum AnalyzerMessage {
+    ChooseFile(usize),
+    ChooseEvent(liquid_exchange::LiquidExchangeEvents),
+}
+
+pub enum AnalyzerScreen {
+    Start,
+}
 
 impl Application for AnalyzerApp {
     type Message = AnalyzerMessage;
@@ -21,10 +29,23 @@ impl Application for AnalyzerApp {
     type Flags = ();
 
     fn new(_flags: ()) -> (Self, Command<AnalyzerMessage>) {
+        // Read files from a specific directory and populate available_files
+        let files = std::fs::read_dir("analysis/static_volatilities/")
+            .unwrap()
+            .filter_map(|entry| {
+                let entry = entry.unwrap();
+                if entry.metadata().unwrap().is_file() {
+                    Some(entry.path())
+                } else {
+                    None
+                }
+            })
+            .collect();
+
         (
             AnalyzerApp {
                 selected_file: None,
-                available_files: vec![],
+                available_files: files,
                 selected_events: vec![],
                 available_events: vec![],
             },
@@ -37,11 +58,41 @@ impl Application for AnalyzerApp {
     }
 
     fn update(&mut self, message: AnalyzerMessage) -> Command<AnalyzerMessage> {
+        match message {
+            AnalyzerMessage::ChooseFile(selected) => {
+                self.selected_file = Some(selected);
+            }
+            _ => {}
+        }
         Command::none()
     }
 
     fn view(&self) -> Element<AnalyzerMessage> {
-        todo!()
+        // Base container for the Running state
+        let mut content = self::column![];
+
+        for (index, file) in self.available_files.iter().enumerate() {
+            // Using a radio button for file selection
+            let file_string = file.display().to_string();
+            let radio = Radio::new(
+                &file_string, // use string representation
+                index,
+                self.selected_file,
+                AnalyzerMessage::ChooseFile,
+            );
+
+            content = content.push(radio);
+        }
+
+        // File selection
+
+        // Finalize
+        container(content)
+            .center_x()
+            .center_y()
+            .width(Length::Fill)
+            .height(Length::Fill)
+            .into()
     }
 
     fn theme(&self) -> Theme {
