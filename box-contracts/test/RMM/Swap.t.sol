c2a117a8281952ac6a412d0993840916d0c13413
clemlak <clementlakhal@gmail.com>
2023-11-01 18:10:57 +0400

    test: update swap tests

--- box-contracts/test/RMM/Swap.t.sol
+++ box-contracts/test/RMM/Swap.t.sol
@@ -4,11 +4,37 @@ pragma solidity ^0.8.13;
 import "./SetUp.t.sol";
 
 contract RMMSwap is RMMSetUp {
-    function test_rmm_swap() public {
-        uint256 amountX = 5_000 ether;
-        rmm.initExactX(amountX, initialPrice);
+    function test_rmm_swap_UpdatesReserves() public {
+        uint256 deltaX = 500 ether;
+
+        rmm.initExactX(5_000 ether, initialPrice);
+
+        uint256 preReserveX = rmm.reserveX();
+        uint256 preReserveY = rmm.reserveY();
 
         uint256 amountY = rmm.swap(500 ether);
-        console.log(amountY);
+
+        uint256 postReserveX = rmm.reserveX();
+        uint256 postReserveY = rmm.reserveY();
+
+        assertEq(preReserveX + deltaX, postReserveX);
+        assertEq(preReserveY - amountY, postReserveY);
+    }
+
+    function test_rmm_swap_UpdatesRMMBalances() public {
+        uint256 deltaX = 500 ether;
+
+        rmm.initExactX(5_000 ether, initialPrice);
+
+        uint256 preBalanceX = tokenX.balanceOf(address(rmm));
+        uint256 preBalanceY = tokenY.balanceOf(address(rmm));
+
+        uint256 amountY = rmm.swap(deltaX);
+
+        uint256 postBalanceX = tokenX.balanceOf(address(rmm));
+        uint256 postBalanceY = tokenY.balanceOf(address(rmm));
+
+        assertEq(preBalanceX + deltaX, postBalanceX);
+        assertEq(preBalanceY - amountY, postBalanceY);
     }
 }
