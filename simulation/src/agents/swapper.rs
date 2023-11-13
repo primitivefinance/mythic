96a83d6410109979a4fc5419949c07c32e9a7dc8
Colin Roberts <colin@primitive.xyz>
2023-11-13 15:48:23 +0300

    feat: swapper
    Swapper still needs to be tested

--- simulation/src/agents/swapper.rs
+++ simulation/src/agents/swapper.rs
@@ -1,4 +1,6 @@
-use super::*;
+use ethers::utils::{parse_units, ParseUnits};
+
+use super::{block_admin::BlockAdmin, price_changer::PriceChanger, token_admin::TokenAdmin, *};
 use crate::settings::parameters::LinspaceParameters;
 
 pub struct Swapper {
@@ -7,6 +9,7 @@ pub struct Swapper {
     pub input_token: Address,
     pub swap_times: Vec<U256>,
     pub amount_in: U256,
+    pub swap_index: usize,
 }
 
 #[derive(Copy, Clone, Debug, Serialize, Deserialize)]
@@ -36,44 +39,100 @@ impl Swapper {
     async fn new(
         environment: &Environment,
         config: &SimulationConfig<Single>,
+        label: &str,
+        price_changer: &PriceChanger,
+        token_admin: &TokenAdmin,
         liquid_exchange_address: Address,
     ) -> Result<Self> {
-        todo!()
-        // let parameters = config.swapper.unwrap();
-        // let client = RevmMiddleware::new(&environment,
-        // "time_based_swapper".into()).unwrap(); let liquid_exchange =
-        // LiquidExchange::new(liquid_exchange_address, client.clone());
-        // let input_token = if parameters.swap_direction {
-        //     liquid_exchange.arbiter_token_x().call().await?
-        // } else {
-        //     liquid_exchange.arbiter_token_x().call().await?
-        // };
-        // let swap_times = linspace!(
-        //     config.trajectory.t_0.0,
-        //     config.trajectory.t_n.0,
-        //     config.trajectory.num_steps
-        // );
-        // let amount_in = parameters.initial_balance / parameters.num_swaps;
-        // Ok(Self {
-        //     client,
-        //     liquid_exchange,
-        //     input_token,
-        //     swap_times,
-        //     amount_in,
-        // })
+        let client = RevmMiddleware::new(environment, Some(label))?;
+        let liquid_exchange = LiquidExchange::new(liquid_exchange_address, client.clone());
+
+        if let Some(AgentParameters::Swapper(params)) = config.agent_parameters.get(label).cloned()
+        {
+            let input_token = if params.swap_direction {
+                token_admin
+                    .arbx
+                    .mint(client.address(), params.initial_balance)
+                    .send()
+                    .await?
+                    .await?;
+                token_admin
+                    .arbx
+                    .approve(client.address(), params.initial_balance)
+                    .send()
+                    .await?
+                    .await?;
+                liquid_exchange.arbiter_token_x().call().await?
+            } else {
+                token_admin
+                    .arby
+                    .mint(client.address(), params.initial_balance)
+                    .send()
+                    .await?
+                    .await?;
+                token_admin
+                    .arby
+                    .approve(client.address(), params.initial_balance)
+                    .send()
+                    .await?
+                    .await?;
+                liquid_exchange.arbiter_token_y().call().await?
+            };
+
+            let swap_times = linspace!(
+                price_changer.trajectory.times[0],
+                price_changer.trajectory.times.last().unwrap().clone(),
+                params.num_swaps.0 as usize
+            );
+            trace!(
+                "Swapper will be tendering token with address: {:?}",
+                input_token
+            );
+
+            let swap_times = swap_times
+                .iter()
+                .map(|time| parse_units(time.to_string(), "wei").unwrap())
+                .map(|unit| {
+                    if let ParseUnits::U256(amount) = unit {
+                        amount
+                    } else {
+                        unreachable!()
+                    }
+                })
+                .collect();
+            trace!("Swap times for the swapper are: {:?}", swap_times);
+
+            let amount_in = params.initial_balance / params.num_swaps.0 as u128;
+            trace!(
+                "Amount in for each of the swapper's swaps is: {}",
+                amount_in
+            );
+
+            Ok(Self {
+                client,
+                liquid_exchange,
+                input_token,
+                swap_times,
+                amount_in,
+                swap_index: 0,
+            })
+        } else {
+            Err(anyhow::anyhow!("No agent parameters found for {}", label))
+        }
     }
 }
 
 #[async_trait::async_trait]
 impl Agent for Swapper {
-    // async fn step(&mut self) -> Result<()> {
-    //     if self.client.get_block_timestamp().await? == self.next_swap_time {
-    //         self.liquid_exchange.swap(self.input_token, self.amount_in)
-    //     }
-    //     Ok(())
-    // }
-
-    // async fn startup(&mut self) -> Result<()> {
-    //     self.0.startup().await
-    // }
+    async fn step(&mut self) -> Result<()> {
+        if self.client.get_block_timestamp().await? >= self.swap_times[self.swap_index] {
+            self.liquid_exchange
+                .swap(self.input_token, self.amount_in)
+                .send()
+                .await?
+                .await?;
+            self.swap_index += 1;
+        }
+        Ok(())
+    }
 }
