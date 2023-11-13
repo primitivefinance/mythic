2d250b4c93ac80710033a96ed0ec5f0827a3a850
Kinrezc <matt.czernik@gmail.com>
2023-11-13 15:26:43 -0500

    broadcast shutdown signal to environment when logs are written to file

--- simulation/src/simulations/dynamic_weights.rs
+++ simulation/src/simulations/dynamic_weights.rs
@@ -57,8 +57,8 @@ pub async fn setup(
         .file_name(config.output_file_name.clone().unwrap())
         .add(price_changer.liquid_exchange.events(), "lex")
         .add(weight_changer.g3m().events(), "g3m")
-        .metadata(config)
-        .map_err(|e| SimulationError::GenericError(e.to_string()))?
+        //        .metadata(config)
+        //        .map_err(|e| SimulationError::GenericError(e.to_string()))?
         .run()
         .map_err(|e| SimulationError::GenericError(e.to_string()))?;
     let steps = price_changer.trajectory.paths[0].len() - 1;
