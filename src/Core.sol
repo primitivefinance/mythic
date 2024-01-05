147058ed2289cee9db1b91b40dce68470e817066
clemlak <clementlakhal@gmail.com>
2024-01-05 15:33:16 +0400

    feat: add allocate and deallocate to Core

--- src/Core.sol
+++ src/Core.sol
@@ -14,9 +14,19 @@ interface NewCore {
     error Invalid(bool negative, int256 swapConstantGrowth);
 
     event Init(address account, uint256 x, uint256 y, uint256 L);
+
+    event Allocate(
+        address indexed account, uint256 deltaX, uint256 deltaY, uint256 deltaL
+    );
+
+    event Deallocate(
+        address indexed account, uint256 deltaX, uint256 deltaY, uint256 deltaL
+    );
 }
 
 contract Core is NewCore {
+    using FixedPointMathLib for uint256;
+
     address public immutable strategy;
     address public immutable tokenX;
     address public immutable tokenY;
@@ -91,5 +101,83 @@ contract Core is NewCore {
         emit Init(msg.sender, x, y, L);
 
         return (x, y, L);
+    }
+
+    function allocate(bytes calldata data)
+        public
+        lock
+        initialized
+        returns (uint256, uint256, uint256)
+    {
+        (uint256 deltaX, uint256 deltaY, uint256 deltaL) =
+            _updatePool(true, data);
+
+        _updateBalance();
+
+        balanceOf[msg.sender] += deltaL;
+        lastFeeGrowthOf[msg.sender] = feeGrowth;
+
+        SafeTransferLib.safeTransferFrom(
+            ERC20(tokenX), msg.sender, address(this), deltaX
+        );
+
+        SafeTransferLib.safeTransferFrom(
+            ERC20(tokenY), msg.sender, address(this), deltaY
+        );
+
+        emit Allocate(msg.sender, deltaX, deltaY, deltaL);
+        return (deltaX, deltaY, deltaL);
+    }
+
+    function deallocate(bytes calldata data)
+        public
+        lock
+        initialized
+        returns (uint256, uint256, uint256)
+    {
+        (uint256 deltaX, uint256 deltaY, uint256 deltaL) =
+            _updatePool(false, data);
+
+        _updateBalance();
+
+        balanceOf[msg.sender] -= deltaL;
+        lastFeeGrowthOf[msg.sender] = feeGrowth;
+
+        SafeTransferLib.safeTransfer(ERC20(tokenX), msg.sender, deltaX);
+
+        SafeTransferLib.safeTransfer(ERC20(tokenY), msg.sender, deltaY);
+
+        emit Deallocate(msg.sender, deltaX, deltaY, deltaL);
+        return (deltaX, deltaY, deltaL);
+    }
+
+    function _updatePool(
+        bool isAllocate,
+        bytes calldata data
+    ) private returns (uint256, uint256, uint256) {
+        (bool valid, int256 invariant, uint256 rx, uint256 ry, uint256 L) =
+            IStrategy(strategy).validateAllocateOrDeallocate(data);
+
+        if (!valid) revert Invalid(invariant < 0, invariant);
+
+        uint256 deltaX = isAllocate ? rx - reserveXWad : reserveXWad - rx;
+        uint256 deltaY = isAllocate ? ry - reserveYWad : reserveYWad - ry;
+        uint256 deltaL = isAllocate ? L - totalLiquidity : totalLiquidity - L;
+
+        reserveXWad = rx;
+        reserveYWad = ry;
+        totalLiquidity = L;
+
+        return (deltaX, deltaY, deltaL);
+    }
+
+    function _updateBalance() private {
+        if (
+            balanceOf[msg.sender] > 0
+                && feeGrowth != lastFeeGrowthOf[msg.sender]
+        ) {
+            uint256 growth = feeGrowth.mulWadDown(lastFeeGrowthOf[msg.sender]);
+            balanceOf[msg.sender] = balanceOf[msg.sender].mulWadDown(growth);
+        }
     }
 }
