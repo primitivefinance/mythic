3047b61e415d71288d05ba3e283c1aa517c82327
clemlak <clementlakhal@gmail.com>
2024-01-11 12:27:10 +0400

    test: update G3M init test

---
+++ src/test/G3M/Init.t.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "./SetUp.sol";
+
+contract G3MInitTest is G3MSetUp {
+    function test_G3M_init_StoresPoolParameters() public init {
+        (
+            bool inited,
+            address controller,
+            address strategy,
+            address tokenX,
+            address tokenY,
+            uint256 reserveX,
+            uint256 reserveY,
+            uint256 totalLiquidity,
+            uint256 feeGrowth
+        ) = dfmm.pools(G3M_POOL_ID);
+
+        assertEq(inited, true);
+        assertEq(controller, address(this));
+        assertEq(strategy, address(g3m));
+        assertEq(tokenX, address(tokenX));
+        assertEq(tokenY, address(tokenY));
+        assertEq(reserveX, defaultReserveX);
+        assertEq(reserveY, reserveY);
+        assertEq(totalLiquidity, totalLiquidity);
+        assertEq(feeGrowth, FixedPointMathLib.WAD);
+    }
+}
