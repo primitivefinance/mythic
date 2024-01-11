edf95d8f507c697ee95d3ccf74f33e8ebcd9f14c
clemlak <clementlakhal@gmail.com>
2024-01-11 15:59:40 +0400

    test: add MockStrategy

---
+++ src/test/helpers/MockStrategy.sol
@@ -0,0 +1,64 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "../../interfaces/IMultiCore.sol";
+import "../../interfaces/IMultiStrategy.sol";
+
+contract MockStrategy is IMultiStrategy {
+    IMultiCore public immutable core;
+
+    constructor(address _core) {
+        core = IMultiCore(_core);
+    }
+
+    function init(
+        uint256 poolId,
+        bytes calldata data
+    )
+        external
+        returns (
+            bool valid,
+            int256 swapConstantGrowth,
+            uint256 reserveX,
+            uint256 reserveY,
+            uint256 totalLiquidity
+        )
+    { }
+
+    function validateAllocateOrDeallocate(
+        uint256 poolId,
+        bytes calldata data
+    )
+        external
+        view
+        returns (
+            bool valid,
+            int256 invariant,
+            uint256 reserveX,
+            uint256 reserveY,
+            uint256 totalLiquidity
+        )
+    { }
+
+    function validateSwap(
+        uint256 poolId,
+        bytes calldata data
+    )
+        external
+        view
+        returns (
+            bool valid,
+            int256 swapConstantGrowth,
+            int256 liquidityDelta,
+            uint256 reserveX,
+            uint256 reserveY,
+            uint256 totalLiquidity
+        )
+    { }
+
+    function dynamicSlot(uint256 poolId)
+        external
+        view
+        returns (bytes memory data)
+    { }
+}
