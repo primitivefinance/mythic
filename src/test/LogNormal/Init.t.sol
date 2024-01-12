a785f1f123ca0b4e26b734b715903f450bc30412
clemlak <clementlakhal@gmail.com>
2024-01-12 18:12:05 +0400

    test: rename LogNormal init tests prefix

--- src/test/LogNormal/Init.t.sol
+++ src/test/LogNormal/Init.t.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.13;
 import "./SetUp.sol";
 
 contract LogNormalInitTest is LogNormalSetUp {
-    function test_LOG_NORMAL_init_StoresPoolParameters() public init {
+    function test_LogNormal_init_StoresPoolParameters() public init {
         (
             bool inited,
             address controller,
@@ -28,7 +28,7 @@ contract LogNormalInitTest is LogNormalSetUp {
         assertEq(feeGrowth, FixedPointMathLib.WAD);
     }
 
-    function test_LOG_NORMAL_init_RevertsIfInvalidTokens() public {
+    function test_LogNormal_init_RevertsIfInvalidTokens() public {
         IMultiCore.InitParams memory initParams = IMultiCore.InitParams({
             strategy: address(logNormal),
             tokenX: address(tokenX),
