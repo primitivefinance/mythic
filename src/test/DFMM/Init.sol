12774778fba5aace8467e4116522788ab652d574
clemlak <clementlakhal@gmail.com>
2024-01-16 19:17:03 +0400

    test: add initial reserves check

--- src/test/DFMM/Init.sol
+++ src/test/DFMM/Init.sol
@@ -6,6 +6,25 @@ import "../../interfaces/IDFMM.sol";
 import "./SetUp.sol";
 
 contract DFMMInit is DFMMSetUp {
+    function test_DFMM_init_StoresStrategyInitialReserves() public {
+        bytes memory data = abi.encode(uint256(1));
+
+        IDFMM.InitParams memory params = IDFMM.InitParams({
+            strategy: address(strategy),
+            tokenX: address(0xbeef),
+            tokenY: address(0xdead),
+            data: data
+        });
+
+        (uint256 poolId,,,) = dfmm.init(params);
+        (uint256 reserveX, uint256 reserveY, uint256 totalLiquidity) =
+            dfmm.getReservesAndLiquidity(poolId);
+
+        assertEq(reserveX, 2 ether);
+        assertEq(reserveY, 3 ether);
+        assertEq(totalLiquidity, 4 ether);
+    }
+
     function test_DFMM_init_RevertsWhenSameTokens() public {
         IDFMM.InitParams memory params = IDFMM.InitParams({
             strategy: address(strategy),
@@ -22,11 +41,13 @@ contract DFMMInit is DFMMSetUp {
         IDFMM.InitParams memory params = IDFMM.InitParams({
             strategy: address(strategy),
             tokenX: address(0xbeef),
-            tokenY: address(0xbeef),
-            data: ""
+            tokenY: address(0xdead),
+            data: abi.encode(uint256(0))
         });
 
-        vm.expectRevert(IDFMM.InvalidTokens.selector);
+        vm.expectRevert(
+            abi.encodeWithSelector(IDFMM.Invalid.selector, false, 0)
+        );
         dfmm.init(params);
     }
 }
