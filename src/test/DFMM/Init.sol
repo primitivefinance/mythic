60181cdda9a470001e90df1404d8296b4db32f34
clemlak <clementlakhal@gmail.com>
2024-01-16 19:26:46 +0400

    test: add DFMM initial reserves transfer check

--- src/test/DFMM/Init.sol
+++ src/test/DFMM/Init.sol
@@ -25,6 +25,33 @@ contract DFMMInit is DFMMSetUp {
         assertEq(totalLiquidity, 4 ether);
     }
 
+    function test_DFMM_init_TransfersInitialReserves() public {
+        bytes memory data = abi.encode(uint256(1));
+
+        IDFMM.InitParams memory params = IDFMM.InitParams({
+            strategy: address(strategy),
+            tokenX: address(tokenX),
+            tokenY: address(tokenY),
+            data: data
+        });
+
+        uint256 dfmmPreBalanceX = tokenX.balanceOf(address(dfmm));
+        uint256 dfmmPreBalanceY = tokenY.balanceOf(address(dfmm));
+
+        uint256 tokenXPreBalance = tokenX.balanceOf(address(this));
+        uint256 tokenYPreBalance = tokenY.balanceOf(address(this));
+
+        (uint256 poolId,,,) = dfmm.init(params);
+        (uint256 reserveX, uint256 reserveY,) =
+            dfmm.getReservesAndLiquidity(poolId);
+
+        assertEq(tokenX.balanceOf(address(dfmm)), dfmmPreBalanceX + reserveX);
+        assertEq(tokenY.balanceOf(address(dfmm)), dfmmPreBalanceY + reserveY);
+
+        assertEq(tokenX.balanceOf(address(this)), tokenXPreBalance - reserveX);
+        assertEq(tokenY.balanceOf(address(this)), tokenYPreBalance - reserveY);
+    }
+
     event Init(
         address indexed account,
         address indexed strategy,
