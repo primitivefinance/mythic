fd3771b7eb36eb0ca684845fc7e8b212103463b3
clemlak <clementlakhal@gmail.com>
2024-01-16 19:22:39 +0400

    test: add Init event check

--- src/test/DFMM/Init.sol
+++ src/test/DFMM/Init.sol
@@ -25,6 +25,33 @@ contract DFMMInit is DFMMSetUp {
         assertEq(totalLiquidity, 4 ether);
     }
 
+    event Init(
+        address indexed account,
+        address indexed strategy,
+        uint256 poolId,
+        uint256 reserveX,
+        uint256 reserveY,
+        uint256 totalLiquidity
+    );
+
+    function test_DFMM_init_EmitsInitEvent() public {
+        bytes memory data = abi.encode(uint256(1));
+
+        IDFMM.InitParams memory params = IDFMM.InitParams({
+            strategy: address(strategy),
+            tokenX: address(0xbeef),
+            tokenY: address(0xdead),
+            data: data
+        });
+
+        vm.expectEmit(true, true, true, true, address(dfmm));
+        emit Init(
+            address(this), address(strategy), 0, 2 ether, 3 ether, 4 ether
+        );
+
+        dfmm.init(params);
+    }
+
     function test_DFMM_init_RevertsWhenSameTokens() public {
         IDFMM.InitParams memory params = IDFMM.InitParams({
             strategy: address(strategy),
