38ccf2c04575982b2d677b16e8ca2525ff85a0c2
clemlak <clementlakhal@gmail.com>
2024-01-17 19:01:23 +0400

    test: add dynamic param value increases check

--- src/test/DynamicParamLib.t.sol
+++ src/test/DynamicParamLib.t.sol
@@ -21,4 +21,16 @@ contract DynamicParamLibTest is Test {
         vm.warp(block.timestamp + 1 days);
         assertEq(param.actualized(), 100);
     }
+
+    function test_DynamicParamLib_actualized_ValueIncreasesOverTime() public {
+        DynamicParam memory param = DynamicParam({
+            lastComputedValue: 1,
+            updateEnd: 10,
+            lastUpdateAt: 0,
+            updatePerSecond: 1
+        });
+
+        vm.warp(5);
+        assertEq(param.actualized(), 6);
+    }
 }
