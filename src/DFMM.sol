d9db72dabcb98465c8034ca62b3e3902866243b5
clemlak <clementlakhal@gmail.com>
2024-01-18 14:29:21 +0400

    chore: remove unused ERC20 import

--- src/DFMM.sol
+++ src/DFMM.sol
@@ -1,7 +1,6 @@
 /// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-import "solmate/tokens/ERC20.sol";
 import "solmate/utils/FixedPointMathLib.sol";
 import "solmate/utils/SafeTransferLib.sol";
 import "solstat/Units.sol";
