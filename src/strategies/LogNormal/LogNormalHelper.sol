e6af369167e04493e77acb652dbcd534c31340a1
clemlak <clementlakhal@gmail.com>
2024-01-18 11:56:29 +0400

    feat: add LogNormalHelper

---
+++ src/strategies/LogNormal/LogNormalHelper.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "./LogNormal.sol";
+
+enum UpdateCode {
+    SwapFee
+}
+
+contract LogNormalHelper {
+    LogNormal public immutable logNormal;
+
+    constructor(address logNormal_) {
+        logNormal = LogNormal(logNormal_);
+    }
+
+    function getPoolParams(uint256 poolId)
+        public
+        view
+        returns (LogNormal.PublicParams memory params)
+    {
+        bytes memory data = logNormal.getPoolParams(poolId);
+        params = abi.decode(data, (LogNormal.PublicParams));
+    }
+}
