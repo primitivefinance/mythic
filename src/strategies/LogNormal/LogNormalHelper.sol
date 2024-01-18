5401bed07e86e3b209ec960c9930089fba72b213
clemlak <clementlakhal@gmail.com>
2024-01-18 18:13:01 +0400

    feat: add LogNormal params update encoding / decoding functions

--- src/strategies/LogNormal/LogNormalHelper.sol
+++ src/strategies/LogNormal/LogNormalHelper.sol
@@ -4,7 +4,46 @@ pragma solidity ^0.8.13;
 import "./LogNormal.sol";
 
 enum UpdateCode {
-    SwapFee
+    SwapFee,
+    Strike,
+    Sigma,
+    Tau
+}
+
+function encodeFeeUpdate(uint256 swapFee) pure returns (bytes memory) {
+    return abi.encode(UpdateCode.SwapFee, uint256(swapFee));
+}
+
+function decodeFeeUpdate(bytes memory data) pure returns (uint256) {
+    (, uint256 swapFee) = abi.decode(data, (UpdateCode, uint256));
+    return swapFee;
+}
+
+function encodeStrikeUpdate(uint256 strike) pure returns (bytes memory) {
+    return abi.encode(UpdateCode.Strike, uint256(strike));
+}
+
+function decodeStrikeUpdate(bytes memory data) pure returns (uint256) {
+    (, uint256 strike) = abi.decode(data, (UpdateCode, uint256));
+    return strike;
+}
+
+function encodeSigmaUpdate(uint256 sigma) pure returns (bytes memory) {
+    return abi.encode(UpdateCode.Sigma, uint256(sigma));
+}
+
+function decodeSigmaUpdate(bytes memory data) pure returns (uint256) {
+    (, uint256 sigma) = abi.decode(data, (UpdateCode, uint256));
+    return sigma;
+}
+
+function encodeTauUpdate(uint256 tau) pure returns (bytes memory) {
+    return abi.encode(UpdateCode.Tau, uint256(tau));
+}
+
+function decodeTauUpdate(bytes memory data) pure returns (uint256) {
+    (, uint256 tau) = abi.decode(data, (UpdateCode, uint256));
+    return tau;
 }
 
 contract LogNormalHelper {
