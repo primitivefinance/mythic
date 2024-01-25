2711447bcd099281d57f5f8e2e8fa66e691ad9c1
clemlak <clementlakhal@gmail.com>
2024-01-25 20:47:47 +0400

    test: add LPToken tests

---
+++ src/test/unit/LPToken/LPToken.t.sol
@@ -0,0 +1,31 @@
+/// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import "src/LPToken.sol";
+
+contract LPTokenTest is Test {
+    LPToken lpToken;
+
+    function setUp() public {
+        lpToken = new LPToken();
+    }
+
+    function test_LPToken_initialize_InitializesTheContract() public {
+        string memory name = "LPToken";
+        string memory symbol = "LPT";
+        lpToken.initialize(name, symbol);
+
+        assertEq(lpToken.name(), name);
+        assertEq(lpToken.symbol(), symbol);
+        assertEq(lpToken.initialized(), true);
+    }
+
+    function test_LPToken_initialize_OnlyInitializesOnce() public {
+        string memory name = "LPToken";
+        string memory symbol = "LPT";
+        lpToken.initialize(name, symbol);
+        vm.expectRevert("Already initialized");
+        lpToken.initialize(name, symbol);
+    }
+}
