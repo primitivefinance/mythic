9731e7564086c0de17e0ab0488f7dc4f75d0f464
clemlak <clementlakhal@gmail.com>
2024-02-15 11:20:14 +0400

    build: add solvers to deployment script

--- src/script/Deployment.s.sol
+++ src/script/Deployment.s.sol
@@ -4,7 +4,9 @@ pragma solidity ^0.8.13;
 import "forge-std/Script.sol";
 import "src/DFMM.sol";
 import "src/strategies/G3M/G3M.sol";
+import "src/solvers/G3M/G3MSolver.sol";
 import "src/strategies/LogNormal/LogNormal.sol";
+import "src/solvers/LogNormal/LogNormalSolver.sol";
 
 contract DeploymentScript is Script {
     function run() public {
@@ -13,11 +15,16 @@ contract DeploymentScript is Script {
 
         DFMM dfmm = new DFMM();
         G3M g3m = new G3M(address(dfmm));
+        G3MSolver g3mSolver = new G3MSolver(address(g3m));
         LogNormal logNormal = new LogNormal(address(dfmm));
+        LogNormalSolver logNormalSolver =
+            new LogNormalSolver(address(logNormal));
 
         console.log("DFMM:", address(dfmm));
         console.log("G3M:", address(g3m));
+        console.log("G3MSolver:", address(g3mSolver));
         console.log("LogNormal:", address(logNormal));
+        console.log("LogNormalSolver:", address(logNormalSolver));
 
         vm.stopBroadcast();
     }
